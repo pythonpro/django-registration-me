3314b24ef25b8f2cfbfec50b4bbee5d9740fdd3c
Roman <pythonprogrammer@mail.ru>
2013-05-28 14:20:05 +0400

    Fixed Django==1.5 incompatibility. Broken Django<=1.2 compatibility.

--- registration/urls.py
+++ registration/urls.py
@@ -21,7 +21,7 @@ various steps of the user-signup process.
 
 
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 
 from registration.forms import PasswordResetForm
@@ -68,7 +68,7 @@ urlpatterns = patterns('',
         register,
         name='registration_register'),
     url(r'^register/complete/$',
-        direct_to_template,
-        {'template': 'registration/registration_complete.html'},
+        TemplateView.as_view(template_name="registration/registration_"
+                                           "complete.html"),
         name='registration_complete'),
 )
